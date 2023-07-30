3a41a567650a64fc34f0d93d656d837463cdfee1
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2023-07-30 20:36:59 +0530

    Create Main.java
    Update Main.java String buffer example

---
+++ src/string/buffer/Main.java
@@ -0,0 +1,27 @@
+package string.buffer;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        //constructor 1
+        StringBuffer sb=new StringBuffer();
+        sb.append("Hello Sagar Kumar");
+        sb.append(" nice");
+
+        sb.insert(5,"rahul");
+        String str=sb.toString();
+        System.out.println(str);
+
+        //constructor2
+        StringBuffer sb2=new StringBuffer("sagar kumar");
+
+        //constructor 3
+        StringBuffer sb3=new StringBuffer(30);
+
+
+    }
+
+
+}
