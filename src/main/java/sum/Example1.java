fc995639bd6c399eba55683f2637fb595b759896
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-06-08 21:38:26 +0530

    Create Example1.java

---
+++ src/main/java/sum/Example1.java
@@ -0,0 +1,18 @@
+package sum;
+
+import java.util.Arrays;
+import java.util.OptionalDouble;
+
+public class Example1 {
+    public static void main(String[] args) {
+
+        int[] arr={1,2,3,4,5};
+
+        int sum = Arrays.stream(arr).sum();
+        System.out.println(sum);
+        
+        OptionalDouble average = Arrays.stream(arr).average();
+        System.out.println(average);
+
+    }
+}
