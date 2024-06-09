3d3f235bd2a21221cefdf9dd927eb8d8d191447c
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-06-09 13:35:25 +0530

    Create Main.java

---
+++ src/main/java/interview/example/Main.java
@@ -0,0 +1,42 @@
+package interview.example;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Student> list =
+                Arrays.asList(
+                        new Student(1, "Rohit", "Mall", 30, "Male", "Mechanical Engineering", 2015, "Mumbai", 122),
+
+                        new Student(2, "Pulkit", "Singh", 56, "Male", "Computer Engineering", 2018, "Delhi", 67),
+
+                        new Student(3, "Ankit", "Patil", 25, "Female", "Mechanical Engineering", 2019, "Kerala", 164),
+
+                        new Student(4, "Satish Ray", "Malaghan", 30, "Male", "Mechanical Engineering", 2014, "Kerala", 26),
+
+                        new Student(5, "Roshan", "Mukd", 23, "Male", "Biotech Engineering", 2022, "Mumbai", 12),
+
+                        new Student(6, "Chetan", "Star", 24, "Male", "Mechanical Engineering", 2023, "Karnataka", 90),
+
+                        new Student(7, "Arun", "Vittal", 26, "Male", "Electronics Engineering", 2014, "Karnataka", 324),
+
+                        new Student(8, "Nam", "Dev", 31, "Male", "Computer Engineering", 2014, "Karnataka", 433),
+
+                        new Student(9, "Sonu", "Shankar", 27, "Female", "Computer Engineering", 2018, "Karnataka", 7),
+
+                        new Student(10, "Shubham", "Pandey", 26, "Male", "Instrumentation Engineering", 2017, "Mumbai", 98));
+
+
+
+
+        // Group The Student By Department Names
+
+        Map<Integer, List<Student>> collect = list.stream().
+                collect(Collectors.groupingBy(Student::getRank));
+        System.out.println(collect);
+
+    }
+}
