4f7aa1ed61f34063ef61ea016180f84d58a9552a
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-06-16 16:46:13 +0530

    Update MapTraverse.java

--- src/main/java/map/MapTraverse.java
+++ src/main/java/map/MapTraverse.java
@@ -1,7 +1,9 @@
 package map;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class MapTraverse {
     public static void main(String[] args) {
@@ -9,6 +11,12 @@ public class MapTraverse {
         map.put("A", 1);
         map.put("B", 2);
         map.put("C", 3);
+        Collection<Integer> values = map.values();
+        System.out.println(values);
+        Set<String> keys = map.keySet();
+        System.out.println(keys);
+        Set<Map.Entry<String, Integer>> entries = map.entrySet();
+        System.out.println(entries);
 
         map.forEach((key,value)->System.out.println(key+" "+value));
         System.out.println("---------");
