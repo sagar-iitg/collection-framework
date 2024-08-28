1d0d298f8a6f6f2344f81b60aaa0e4f80f67f17f
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-08-28 07:47:26 +0530

    Update Main.java

--- src/main/java/string/buffer/Main.java
+++ src/main/java/string/buffer/Main.java
@@ -6,11 +6,11 @@ public class Main {
     public static void main(String[] args) {
 
         //constructor 1
-        StringBuffer sb=new StringBuffer();
+        StringBuffer sb = new StringBuffer();
         sb.append("Hello Sagar Kumar");
         sb.append(" nice");
 
-        sb.insert(5,"rahul");
+        sb.insert(6,"rahul ");
         String str=sb.toString();
         System.out.println(str);
 
