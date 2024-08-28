76b7e24a8fa3b81451475868b49bd67dd8e7332f
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-08-28 07:33:16 +0530

    Create Student.java

---
+++ src/main/java/list/Student.java
@@ -0,0 +1,22 @@
+package list;
+
+public class Student {
+
+
+    private int rollNo;
+    private String name;
+
+
+    public Student(){}
+
+    public Student(int rollNo, String name) {
+        this.rollNo = rollNo;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "(" +rollNo +
+                "," + name+ ")";
+    }
+}
